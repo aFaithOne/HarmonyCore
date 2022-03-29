8146cd41351def239d59420a11a07dc2c3985a43
Lumenowaty <lumenowaty.dev@gmail.com>
2022-03-29 22:18:40 +0200

    update 28.03.2022

--- src/main/java/me/lumenowaty/harmonycore/components/tasks/HTask.java
+++ src/main/java/me/lumenowaty/harmonycore/components/tasks/HTask.java
@@ -3,6 +3,7 @@ package me.lumenowaty.harmonycore.components.tasks;
 import me.lumenowaty.harmonycore.HarmonyCore;
 import me.lumenowaty.harmonycore.components.interfaces.Taskable;
 import org.bukkit.Bukkit;
+import org.bukkit.scheduler.BukkitRunnable;
 
 
 public abstract class HTask implements Taskable {
@@ -26,6 +27,12 @@ public abstract class HTask implements Taskable {
 
     @Override
     public void setStopTask(Runnable action, int time) {
-        Bukkit.getScheduler().runTaskLaterAsynchronously(HarmonyCore.getInstance(), this::stop, 20L * time);
+        new BukkitRunnable() {
+
+            @Override
+            public void run() {
+                stop();
+            }
+        }.runTaskLaterAsynchronously(HarmonyCore.getInstance(), 20L * time);
     }
 }
